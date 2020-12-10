b92c6ea5930fe24699e92ae381c54e5440bacd6f
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-11 00:27:52 +0100

    enum lookup + config handling

--- src/main/java/leee/nc/LeeesNC.java
+++ src/main/java/leee/nc/LeeesNC.java
@@ -10,39 +10,38 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LeeesNC extends JavaPlugin implements Listener {
 
-    private boolean needPermission;
-
-    private String[] validColorModifiers = new String[]{"AQUA", "BLACK", "BLUE", "DARK_AQUA", "DARK_BLUE", "DARK_GRAY", "DARK_GREEN", "DARK_PURPLE", "DARK_RED", "GOLD", "GRAY", "GREEN", "LIGHT_PURPLE", "RED", "WHITE", "YELLOW"};
-    private List<String> allowedFormatModifiers = new ArrayList<>();
+    private Config config;
+    private Map<String, ChatColor> formatterLookup;
 
     public void onEnable() {
-
-        loadConfig();
-
-        needPermission = getConfig().getBoolean("command-needs-permission");
-
-        if (getConfig().getBoolean("modifier-allowed-bold")) {
-            allowedFormatModifiers.add("BOLD");
-        }
-        if (getConfig().getBoolean("modifier-allowed-italic")) {
-            allowedFormatModifiers.add("ITALIC");
-        }
-        if (getConfig().getBoolean("modifier-allowed-magic")) {
-            allowedFormatModifiers.add("MAGIC");
-        }
-        if (getConfig().getBoolean("modifier-allowed-strikethrough")) {
-            allowedFormatModifiers.add("STRIKETHROUGH");
-        }
-        if (getConfig().getBoolean("modifier-allowed-underline")) {
-            allowedFormatModifiers.add("UNDERLINE");
-        }
-
+        config = new Config();
+        formatterLookup = generateFormatterLookup();
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private Map<String, ChatColor> generateFormatterLookup() {
+
+        Stream<ChatColor> colors = Arrays
+                .stream(ChatColor.values())
+                .filter(ChatColor::isColor);
+
+        Stream<ChatColor> formatters = Arrays
+                .stream(ChatColor.values())
+                .filter(ChatColor::isFormat)
+                .filter(c -> c.equals(ChatColor.BOLD) && config.modifierAllowedBold
+                        || c.equals(ChatColor.ITALIC) && config.modifierAllowedItalic
+                        || c.equals(ChatColor.MAGIC) && config.modifierAllowedMagic
+                        || c.equals(ChatColor.STRIKETHROUGH) && config.modifierAllowedStrikethrough
+                        || c.equals(ChatColor.UNDERLINE) && config.modifierAllowedUnderline);
+
+        return Stream.concat(colors, formatters).collect(Collectors.toMap(e -> e.asBungee().getName().toLowerCase(), e -> e));
 
     }
 
@@ -59,135 +58,84 @@ public class LeeesNC extends JavaPlugin implements Listener {
 
         Player player = (Player) sender;
 
-        if (needPermission && !player.hasPermission("Leee.nc")) {
-            player.sendMessage(ChatColor.YELLOW + "[LeeesNC] " + ChatColor.DARK_AQUA + "You do not have permission to perform this command!");
+        if (config.commandNeedsPermission && !player.hasPermission("leee.nc")) {
+            player.sendMessage(ChatColor.RED + "You do not have permission to perform this command!");
             return false;
         }
 
         if (args.length == 0) {
-            helpMessage(player);
+            // TODO: player.sendMessage(helpMessage);
             return false;
         }
 
-        String userInput = String.join(" ", args).toUpperCase();
-        userInput = userInput.replaceAll("LIGHT-", "LIGHT_").replaceAll("DARK-", "DARK_");
+        String formatters = Arrays.stream(args).map(s -> s.replaceAll("[^a-zA-Z]", "_").toLowerCase())
+                .filter(s -> formatterLookup.containsKey(s))
+                .map(s -> formatterLookup.get(s).toString())
+                .collect(Collectors.joining());
 
-        String usedColorModifier = null;
-
-        for (String color : validColorModifiers) {
-            if (userInput.contains(color)) {
-                usedColorModifier = color;
-                break;
-            }
-        }
-
-        if (usedColorModifier == null || usedColorModifier.isEmpty()) {
-            helpMessage(player);
-            return false;
-        }
-
-        List<String> usedFormatModifiers = new ArrayList<>();
-
-        for (String mod : allowedFormatModifiers) {
-            if (userInput.contains(mod)) {
-                usedFormatModifiers.add(mod);
-            }
-        }
-
-        changePlayerNameColor(player, usedColorModifier, usedFormatModifiers);
-
-        return true;
+        return changeNameColor(player, formatters, true);
 
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
 
-        Player player = event.getPlayer();
-        String modifiers = this.getConfig().getString(String.valueOf(player.getUniqueId()));
-
+        String modifiers = this.getConfig().getString(String.valueOf(event.getPlayer().getUniqueId()));
         if (modifiers != null && !modifiers.isEmpty()) {
-            changePlayerNameColor(player, modifiers, false);
+            changeNameColor(event.getPlayer(), modifiers, false);
         }
 
     }
 
-    private void loadConfig() {
+    private boolean changeNameColor(Player player, String modifiers, boolean notify) {
 
-        getConfig().addDefault("command-needs-permission", false);
-        getConfig().addDefault("modifier-allowed-bold", true);
-        getConfig().addDefault("modifier-allowed-strikethrough", false);
-        getConfig().addDefault("modifier-allowed-underline", false);
-        getConfig().addDefault("modifier-allowed-italic", true);
-        getConfig().addDefault("modifier-allowed-magic", false);
-
-        getConfig().options().copyDefaults(true);
-
-        saveConfig();
-
-    }
-
-    private void changePlayerNameColor(Player player, String color, List<String> formatters) {
-
-        ChatColor colorModifier = ChatColor.valueOf(color);
-
-        List<ChatColor> formatModifier = new ArrayList<>();
-
-        for (String formatter : formatters) {
-            formatModifier.add(ChatColor.valueOf(formatter));
+        if (modifiers.isEmpty()) {
+            return false;
         }
 
-        StringBuilder fullModifiers = new StringBuilder();
-
-        fullModifiers.append(colorModifier);
-
-        for (ChatColor formatter : formatModifier) {
-            fullModifiers.append(formatter);
-        }
-
-        changePlayerNameColor(player, fullModifiers.toString(), true);
-
-    }
-
-    private void changePlayerNameColor(Player player, String modifiers, boolean notify) {
-
-        getLogger().info("Changing name of Player " + player.getName() + " to: " + modifiers + player.getName() + ChatColor.RESET);
-
-        player.setDisplayName(modifiers + player.getName() + ChatColor.RESET);
-
+        String newName = modifiers + player.getName() + ChatColor.RESET;
+        getLogger().info("Changing name of " + player.getName() + " to: " + newName);
+        player.setDisplayName(newName);
         this.getConfig().set(String.valueOf(player.getUniqueId()), modifiers);
         this.saveConfig();
 
         if (notify) {
-            player.sendMessage(ChatColor.YELLOW + "[LeeesNC] " + ChatColor.DARK_AQUA + "Your name is now: " + modifiers + player.getName());
+            player.sendMessage(ChatColor.GOLD + "Your name is now: " + newName);
         }
+
+        return true;
 
     }
 
-    private void helpMessage(Player player) {
+    class Config {
 
-        StringBuilder fancyColorModifiers = new StringBuilder();
-        StringBuilder fancyFormatModifiers = new StringBuilder();
+        final boolean commandNeedsPermission;
+        final boolean modifierAllowedBold;
+        final boolean modifierAllowedItalic;
+        final boolean modifierAllowedMagic;
+        final boolean modifierAllowedStrikethrough;
+        final boolean modifierAllowedUnderline;
 
-        for (String color : validColorModifiers) {
-            fancyColorModifiers.append(ChatColor.valueOf(color));
-            fancyColorModifiers.append(color);
-            fancyColorModifiers.append(ChatColor.RESET);
-            fancyColorModifiers.append(", ");
-        }
+        public Config() {
 
-        player.sendMessage(ChatColor.YELLOW + "[LeeesNC] " + ChatColor.DARK_AQUA + "Please specify a valid color: " + ChatColor.RESET + String.join(", ", fancyColorModifiers.substring(0, fancyColorModifiers.length() - 4)));
+            // defaults
+            getConfig().addDefault("command-needs-permission", false);
+            getConfig().addDefault("modifier-allowed-bold", true);
+            getConfig().addDefault("modifier-allowed-strikethrough", false);
+            getConfig().addDefault("modifier-allowed-underline", false);
+            getConfig().addDefault("modifier-allowed-italic", true);
+            getConfig().addDefault("modifier-allowed-magic", false);
+            getConfig().options().copyDefaults(true);
+            saveConfig();
 
-        if (allowedFormatModifiers.size() > 0) {
+            this.commandNeedsPermission = getConfig().getBoolean("command-needs-permission");
 
-            for (String formatter : allowedFormatModifiers) {
-                fancyFormatModifiers.append(ChatColor.valueOf(formatter));
-                fancyFormatModifiers.append(formatter);
-                fancyFormatModifiers.append(ChatColor.RESET);
-                fancyFormatModifiers.append(", ");
-            }
-
-            player.sendMessage(ChatColor.YELLOW + "[LeeesNC] " + ChatColor.DARK_AQUA + "You can also use Formatters: " + ChatColor.RESET + String.join(", ", fancyFormatModifiers.substring(0, fancyFormatModifiers.length() - 4)));
+            // TODO: permission based formatter usage
+            this.modifierAllowedBold = getConfig().getBoolean("modifier-allowed-bold");
+            this.modifierAllowedItalic = getConfig().getBoolean("modifier-allowed-italic");
+            this.modifierAllowedMagic = getConfig().getBoolean("modifier-allowed-magic");
+            this.modifierAllowedStrikethrough = getConfig().getBoolean("modifier-allowed-strikethrough");
+            this.modifierAllowedUnderline = getConfig().getBoolean("modifier-allowed-underline");
 
         }
 
